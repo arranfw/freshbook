54f5b6d6c74c4429190640c094d4fd28577e8589
arranfw <arran@freshworks.io>
2021-02-18 16:04:32 -0700

    remove unused imports

--- src/stories/accessibility/contrast/failures.stories.tsx
+++ src/stories/accessibility/contrast/failures.stories.tsx
@@ -1,63 +1,10 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faUserFriends } from '@fortawesome/free-solid-svg-icons/faUserFriends';
 import { Table } from '../../components';
 
 const TypographyBase = css`
   font-family: 'Open Sans';
   margin: 0;
-`;
-
-const PassingRegularText = styled.p`
-  ${TypographyBase}
-  color: #757575;
-  font-size: 16px;
-`;
-
-const PassingBoldText = styled.p`
-  ${TypographyBase}
-  color: #949494;
-  font-size: 14px;
-  font-weight: bold;
-`;
-
-const PassingLargeText = styled.p`
-  ${TypographyBase}
-  color: #949494;
-  font-size: 18px;
-`;
-
-const PassingIcon = styled(Icon)`
-  color: #949494;
-`;
-
-const PassingInput = styled.input`
-  border: 1px solid #949494;
-  border-radius: 5px;
-  padding: 0.5rem 1rem;
-  margin: 0;
-  pointer-events: none;
-`;
-
-const DisabledInput = styled.input`
-  border: 1px solid #a3a3a3;
-  border-radius: 5px;
-  padding: 0.5rem 1rem;
-  margin: 0;
-`;
-
-const PassingThinFocus = styled(PassingInput)`
-  box-shadow: 0 0 0 1px #fff, 0 0 0 2px #757575;
-`;
-
-const PassingThickFocus = styled(PassingInput)`
-  box-shadow: 0 0 0 1px #fff, 0 0 0 4px #949494;
-`;
-
-const AlertStar = styled.span`
-  padding: 0.25rem 0.5rem;
-  color: ${({ theme }) => theme.palette.tones.critical};
 `;
 
 const FailingText = styled.p`
